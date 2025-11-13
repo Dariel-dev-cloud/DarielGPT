dd5f7ea1c68684bc1a4c4470fb2678491631d791
Dariel-dev-cloud <ortegasorzanodariel@gmail.com>
2025-11-13 18:47:30 -0500

    feat: implement user authentication with Login component; enhance App component for user state management

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,12 @@ import { assets } from './assets/assets'
 import './assets/prism.css'
 import Loading from './pages/Loading'
 import { useState } from 'react'
+import { useAppContext } from './context/AppContext'
+import Login from './pages/Login'
 
 export default function App() {
+
+  const { user } = useAppContext()
   const [isMenuOpen, setIsMenuOpen] = useState(false)
   const { pathname } = useLocation()
 
@@ -18,16 +22,25 @@ export default function App() {
     <>
       {!isMenuOpen && <img src={assets.menu_icon} className='absolute top-3 left-3 
      w-8 h-8 cursor-pointer md:hidden not-dark:invert' onClick={() => setIsMenuOpen(true)} />}
-      <div className='dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white ' >
-        <div className='flex h-screen w-screen' >
-          <Sidebar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
-          <Routes>
-            <Route path='/' element={<ChatBox />} />
-            <Route path='/credits' element={<Credits />} />
-            <Route path='/community' element={<Community />} />
-          </Routes>
+
+      {user ? (
+        <div className='dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white ' >
+          <div className='flex h-screen w-screen' >
+            <Sidebar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+            <Routes>
+              <Route path='/' element={<ChatBox />} />
+              <Route path='/credits' element={<Credits />} />
+              <Route path='/community' element={<Community />} />
+            </Routes>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className='bg-gradient-to-b from-[#242124] to-[#000000] flex 
+        items-center justify-center h-screen w-screen ' >
+          <Login />
+        </div>
+      )}
+
 
     </>
   )
